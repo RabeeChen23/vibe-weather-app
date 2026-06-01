41d4c00c97db72db6286f36696fdd9731a218486
RabeeChen23 <cxa4515chn@gmail.com>
2026-06-01 08:58:05 +0800

    Initial commit: Add magic 8 ball script

---
+++ weather.py
@@ -0,0 +1,10 @@
+import random
+
+weather_data = {
+    "北京": {"温度": "22°C", "天气": "晴", "湿度": "45%"},
+    "上海": {"温度": "28°C", "天气": "多云", "湿度": "65%"},
+    "广州": {"温度": "31°C", "天气": "雷阵雨", "湿度": "80%"},
+}
+
+city, info = random.choice(list(weather_data.items()))
+print(""f"{city}: {info['天气']}，{info['温度']}，湿度 {info['湿度']}""")
